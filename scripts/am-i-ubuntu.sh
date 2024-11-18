c893250d838bbc3d3d9950d5e5671fe25ecb340c
Samy Boudjema <samyboudjema@MacBook-Air-de-Samy.local>
2024-11-18 12:17:20 +0100

    Add script 0 and 1

--- scripts/am-i-ubuntu.sh
+++ scripts/am-i-ubuntu.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 RELEASE=$(head -n 1 <  /etc/lsb-release | cut -d '=' -f 2)
 
 if [ "$RELEASE" = "Ubuntu" ]; then
-    echo Good ! It\'s Ubuntu
+    echo C'est un gaté ça !
 else
-    echo Oh no, i\'ts not Ubuntu...
+    echo C'est pas un gaté ça !
 fi
