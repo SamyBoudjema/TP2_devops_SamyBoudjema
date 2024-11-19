db814cc1e76eec6316692ce3018c66fafc748eec
Samy Boudjema <samyboudjema@MacBook-Air-de-Samy.local>
2024-11-19 14:17:39 +0100

    Update am-i-ubuntu.sh : else

--- scripts/am-i-ubuntu.sh
+++ scripts/am-i-ubuntu.sh
@@ -3,7 +3,7 @@
 RELEASE=$(head -n 1 <  /etc/lsb-release | cut -d '=' -f 2)
 
 if [ "$RELEASE" = "Ubuntu" ]; then
-    echo C'est un gaté ça !
+    echo "C'est un gaté ça !"
 else
-    echo C'est pas un gaté ça !
+    echo "C'est pas un gaté ça !"
 fi
